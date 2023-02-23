5ca2b78208d498b20f2dcf0ab11a11c1f52bc0cf
Serhan ÇINAR <serhan6200@gmail.com>
2023-02-23 14:01:56 +0300

    Navbar Part 1

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link as ReactLink } from "react-router-dom";
+import {
+  Box,
+  Flex,
+  Link,
+  Stack,
+  HStack,
+  Icon,
+  IconButton,
+  Text,
+  useDisclosure,
+  useColorMode,
+  useColorModeValue as mode,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { GiWorld } from "react-icons/gi";
+import { FaTwitter, FaYoutube, FaFacebook, FaInstagram } from "react-icons/fa";
+import { MdAdminPanelSettings } from "react-icons/md";
 
 const Navbar = () => {
   return <div>Navbar</div>;
